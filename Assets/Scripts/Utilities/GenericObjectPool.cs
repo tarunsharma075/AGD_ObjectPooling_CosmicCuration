ee8d9b2f9424de045b83cf18a6e942df8b4aeaad
Tarun <tarunkumarsharma38@gmail.com>
2025-12-10 20:25:33 +0530

    added generic pool

--- Assets/Scripts/Utilities/GenericObjectPool.cs
+++ Assets/Scripts/Utilities/GenericObjectPool.cs
@@ -1,21 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 namespace CosmicCuration.Utilities
 {
     public class GenericObjectPool<T> where T : class
     {
 
-    private List<PooledItems<T>> pooledItem= new PooledItems<T>();
+    private List<PooledItems<T>> pooledItem= new List <PooledItems<T>>();   
 
 
-
-
-      public class PooledItems<T>
+    protected T GetItem()
+      {
+       if(pooledItem.Count > 0)
         {
-            private T item;
-            private bool isUsed;
+            PooledItems<T> item = pooledItem.Find(i => !i.isUsed);
+            item.isUsed = true;
+            return item.Item;
+            }
+            return CreatePooledItem();
+      }
+
+
+protected T CreatePooledItem()
+        {
+            PooledItems<T> item = new PooledItems<T>();
+            item.Item = CreatePooledItemk();
+            item.isUsed = true;
+            pooledItem.Add(item);
+            return item.Item;
+        }
+
+        protected virtual  T CreatePooledItemk()
+        {
+            throw new NotImplementedException();
+        }
+
+        public class PooledItems<T>
+        {
+            public T Item;
+            public  bool isUsed;
         }
         
     }
