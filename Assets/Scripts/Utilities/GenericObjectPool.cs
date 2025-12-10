cb4d4767b4415b0e56ff84ac7aa4ceed35f7eb72
Tarun <tarunkumarsharma38@gmail.com>
2025-12-10 20:20:43 +0530

    Implement GetItem() method in Generic Object Pool

---
+++ Assets/Scripts/Utilities/GenericObjectPool.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CosmicCuration.Utilities
+{
+    public class GenericObjectPool<T> where T : class
+    {
+
+    private List<PooledItems<T>> pooledItem= new PooledItems<T>();
+
+
+
+
+      public class PooledItems<T>
+        {
+            private T item;
+            private bool isUsed;
+        }
+        
+    }
+}
